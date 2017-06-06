6d589b2bf1814ee8e88ccd7b2a753a88ad3453fe
avilavate <avilavate123@gmail.com>
2017-06-06 14:10:41 +0530

    Linq=>XML

--- linq-tpl/data-lib/Program.cs
+++ linq-tpl/data-lib/Program.cs
@@ -16,7 +16,7 @@ namespace data_lib
 
             foreach (var car in q)
             {
-         //       Console.WriteLine(car.Name + " : " + car.Combined_FE);
+                //       Console.WriteLine(car.Name + " : " + car.Combined_FE);
             }
             //var q1 = ops.PerformanceGroupByManufacturer();
             //foreach (var perfByM in q1)
@@ -35,12 +35,22 @@ namespace data_lib
 
             var q2 = ops.GetFuelStatistics();
 
-            xml_parser.CreateCarXML();
-        
+            // xml_parser.CreateCarXML();
+            var cars = xml_parser.ParseCarXML();
+            var cars1 = ops.TopNFuelEff(10);
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name + " : " + car.Combined_FE);
+            }
+            Console.WriteLine("-----------------------------------------------");
+            foreach (var car in cars1)
+            {
+                Console.WriteLine(car.Name + " : " + car.Combined_FE);
+            }
 
             Console.WriteLine($"Max: {q1.Max} Min: {q1.Min} Avg: {q1.Avg}");
             Console.WriteLine($"Max: {q2.Max} Min: {q2.Min} Avg: {q2.Avg}");
-        //    Console.WriteLine(ops.GetTotal());
+            //    Console.WriteLine(ops.GetTotal());
 
             Console.Read();
         }
--- linq-tpl/data-lib/xml_parser.cs
+++ linq-tpl/data-lib/xml_parser.cs
@@ -12,7 +12,7 @@ namespace data_lib
     {
 
         //Model Year,Division,Carline,Eng Displ,# Cyl,City FE,Hwy FE,Comb FE
-        public static List<car> CreateCarXML(string path = @"./fuel.csv")
+        public void CreateCarXML(string path = @"./fuel.csv")
         {
             //To Test TPL
             //  Thread.Sleep(3000);
@@ -21,48 +21,51 @@ namespace data_lib
 
             XNamespace ns = "https://avil.com/cars";
             XNamespace xs = "https://avil.com/cars/car";
-            var Cars = new XElement(ns + "Cars", lines.
+            var Cars = new XElement("Cars", lines.
                     Skip(1).
                     Where(line => line.Length > 0).
                     Select(line =>
                     {
                         var record = line.Split(',');
-                        return new XElement(xs + "Car",
-                            new XAttribute("Name", record[1]),
-                            new XAttribute("Year", record[0]),
-                            new XAttribute("CarLine", record[2]),
-                            new XAttribute("Engin_Displacement", record[3]),
-                            new XAttribute("Cly", int.Parse(record[4])),
-                            new XAttribute("City_FE", int.Parse(record[5])),
-                            new XAttribute("Highway_FE", int.Parse(record[6])),
-                            new XAttribute("Combined_FE", int.Parse(record[7]))
+                        return new XElement("Car",
+                            new XElement("Name", record[1]),
+                            new XElement("Year", record[0]),
+                            new XElement("CarLine", record[2]),
+                            new XElement("Engin_Displacement", record[3]),
+                            new XElement("Cly", int.Parse(record[4])),
+                            new XElement("City_FE", int.Parse(record[5])),
+                            new XElement("Highway_FE", int.Parse(record[6])),
+                            new XElement("Combined_FE", int.Parse(record[7]))
                             );
                     }));
 
             xml.Add(Cars);
             xml.Save("cars.xml");
-
-            return null;
         }
 
-        //public static List<manufacturer> ParseManufacturer(string path = @"./manufacturers.csv")
-        //{
-        //    var lines = File.ReadAllLines(path);
+        public static List<car> ParseCarXML(string path = @"cars.xml")
+        {
+            var Cars = XDocument.Load(path);
+            var q = Cars.Element("Cars")?.
+                  Elements("Car")?.
+                  OrderByDescending(x=>int.Parse(x.Element("Combined_FE").Value)).
+                  Take(10).
+                  Select(x =>
+                  {
+                      return new car()
+                      {
+                          Name = x.Element("Name").Value,
+                          Year = x.Element("Year").Value,
+                          CarLine = x.Element("CarLine").Value,
+                          Engin_Displacement = x.Element("Engin_Displacement").Value,
+                          Cly = int.Parse(x.Element("Cly").Value),
+                          City_FE = int.Parse(x.Element("City_FE").Value),
+                          Highway_FE = int.Parse(x.Element("Highway_FE").Value),
+                          Combined_FE = int.Parse(x.Element("Combined_FE").Value)
+                      };
+                  });
 
-        //    var q = lines.
-        //        Where(l => l.Length > 0).
-        //        Select((l) =>
-        //        {
-        //            var record = l.Split(',');
-        //            return (new manufacturer()
-        //            {
-        //                Manufacturer = record[0],
-        //                Country = record[1],
-        //                Year = int.Parse(record[2])
-        //            });
-        //        });
-
-        //    return q.ToList();
-        //}
+            return q.ToList();
+        }
     }
 }
